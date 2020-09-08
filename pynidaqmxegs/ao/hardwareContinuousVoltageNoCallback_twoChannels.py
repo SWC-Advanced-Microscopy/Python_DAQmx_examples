d205f3ca208d6e2180421304e2f3216bcba1542f
Rob Campbell <git@raacampbell.com>
2020-09-08 21:15:31 +0100

    Add two channel example

--- pynidaqmxegs/ao/hardwareContinuousVoltageNoCallback_twoChannels.py
+++ pynidaqmxegs/ao/hardwareContinuousVoltageNoCallback_twoChannels.py
@@ -1,33 +1,36 @@
 '''
- Example showing hardware-timed continuous analog output using a callback function
+ Example showing hardware-timed continuous analog output with two channels without using a callback function
 
- function pynidaqmx.ao.hardwareContinuousVoltageCallback
+ pynidaqmx.ao.hardwareContinuousVoltageNoCallback_twoChannels
 
  Purpose
- Demonstrates how to do hardware-timed continuous analog output using a callback function. 
- This means we will not use sample regeneration. i.e. the buffer contents is play out 
- once only and as it empties it is re-filled using a callback funtion.
+ Demonstrates how to do hardware-timed continuous analog output without a callback function. 
+ To achieve this we use sample regeneration. The buffer contents is played out repeatedly:
+ once the end of the buffer is reached, the DAQ returns to the beginning and resumes from there. 
  This code plays a continuous sine wave out of an analog output channel using the DAQ's 
- internal (on-board) sample clock. The example uses no triggers. The waveform is regenerated 
- continuously using a callback function.
+ internal (on-board) sample clock. The example uses no triggers.
 
 
  Monitoring the output
  If you lack an oscilloscope you may physically connect the analog output to 
  an analog input and monitor this using the NI MAX test panel. You likely will need
  to select RSE: http://www.ni.com/white-paper/3344/en/
- 
+
 
  Example session:
  AO = pynidaqmx.ao.hardwareContinuousVoltageCallback()
  AO.dev_name = 'Dev2' #optionally change device name to something other than Dev1
  AO.create_task()
 
+ Wiring suggestion:
+ If you have no scope connect AO0 to AI0 and AO1 to AI1. Display AI0:1 in the NI MAX AI test panel
+
+
  You can also run from the system command line:
  - cd to path containing the function
- - python hardwareContinuousVoltageCallback.py to run the demo
+ - python hardwareContinuousVoltageNoCallback_twoChannels.py to run the demo
 
- Rob Campbell - SWC, 2020
+  Rob Campbell - SWC, 2020
 
  
  Also see:
@@ -39,14 +42,13 @@ import nidaqmx
 from nidaqmx.constants import (AcquisitionType,RegenerationMode)
 import numpy as np
 
-class hardwareContinuousVoltageCallback():
+class hardwareContinuousVoltageNoCallback_twoChannels():
 
     # Class properties
 
     # Parameters for the acquisition (device and channels)
     dev_name = 'Dev1'      # The name of the DAQ device as shown in MAX
     task_name = 'hardAO'   # A string that will provide a label for the task
-    physical_channel = 0   # A scalar or an array with the channel numbers
 
     # Task configuration
     sample_rate = 5000                 # Sample Rate in Hz
@@ -61,19 +63,13 @@ class hardwareContinuousVoltageCallback():
             self.create_task()
 
 
-    def top_up_buffer(self, hTask, event_type, num_samples, callback_data):
-            '''
-            This method is the callback for the analog output task.
-            It re-fills the output buffer when it is half empty
-            '''
-            self.h_task.write(self.waveform, timeout=5)
-            return 0
-
-
     def create_task(self):
 
-        # Build one cycle of a sine wave and scale to 5V to play through the AO line.
-        self.waveform=np.sin(np.linspace(-np.pi,np.pi, 500))*5
+        # Build one cycle of a sine wave and scale to 5V to play through AO0.
+        # Through AO1 we will play the cosine wave.
+        w0 = np.sin(np.linspace(-np.pi,np.pi, 500))*5
+        w1 = np.cos(np.linspace(-np.pi,np.pi, 500))*5
+        self.waveform=np.stack((w0,w1))
         self.num_samples_per_channel = len(self.waveform)  # The number of samples to be stored in the buffer per channel
         print('Constructed a waveform of length %d that will played at %d samples per second' % \
               (self.num_samples_per_channel, self.sample_rate))
@@ -84,11 +80,11 @@ class hardwareContinuousVoltageCallback():
         self.h_task = nidaqmx.Task(self.task_name)
 
 
-        # * Set up analog output 0 on device defined by variable dev_name
+        # * Set up analog output on channels 0 and 1 on device defined by property self.dev_name
         #   C equivalent - DAQmxCreateAOVoltageChan
         #   http://zone.ni.com/reference/en-XX/help/370471AE-01/daqmxcfunc/daqmxcreateaovoltagechan/
         # https://nidaqmx-python.readthedocs.io/en/latest/ao_channel_collection.html
-        connect_at = '%s/ao%d' % (self.dev_name,self.physical_channel)
+        connect_at = '%s/ao0:1' % self.dev_name
         self.h_task.ao_channels.add_ao_voltage_chan(connect_at)
 
 
@@ -109,8 +105,9 @@ class hardwareContinuousVoltageCallback():
         # For more on DAQmx write properties: http://zone.ni.com/reference/en-XX/help/370469AG-01/daqmxprop/daqmxwrite/
         # For a discussion on regeneration mode in the context of analog output tasks see:
         # https://forums.ni.com/t5/Multifunction-DAQ/Continuous-write-analog-voltage-NI-cDAQ-9178-with-callbacks/td-p/4036271
-        self.h_task.out_stream.regen_mode = RegenerationMode.DONT_ALLOW_REGENERATION
+        self.h_task.out_stream.regen_mode = RegenerationMode.ALLOW_REGENERATION
         print('Regeneration mode is set to: %s' % str(self.h_task.out_stream.regen_mode))
+
 
 
         # * Write the waveform to the buffer with a 5 second timeout in case it fails
@@ -118,11 +115,6 @@ class hardwareContinuousVoltageCallback():
         #   http://zone.ni.com/reference/en-XX/help/370471AG-01/daqmxcfunc/daqmxwriteanalogf64/
         self.h_task.write(self.waveform, timeout=2)
 
-
-        # * Call a function to top up the buffer when half of the samples
-        #   have been played out.
-        run_after_t_samples = round(self.num_samples_per_channel*0.50) # Run when half the signal has been played
-        self.h_task.register_every_n_samples_transferred_from_buffer_event(run_after_t_samples, self.top_up_buffer)
 
         print('\n')
 
@@ -154,11 +146,11 @@ class hardwareContinuousVoltageCallback():
             return False
 
 
+
 if __name__ == '__main__':
-    print('\nRunning demo for hardwareContinuousVoltageCallback\n\n')
-    AO = hardwareContinuousVoltageCallback()
+    print('\nRunning demo for hardwareContinuousVoltageNoCallback_twoChannels\n\n')
+    AO = hardwareContinuousVoltageNoCallback_twoChannels()
     AO.create_task()
-    AO.start_signal()
+    AO.h_task.start()
     input('press return to stop')
-    AO.stop_signal()
     AO.h_task.close()
